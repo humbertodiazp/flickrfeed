e8b8efcf47526a81307ef34e9355a72c6b65633d
Humberto <hdkaratechop@gmail.com>
2023-02-26 23:41:17 -0800

    initialize flickraw

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative "application"
+require "flickraw"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+flickr = FlickRaw::Flickr.new

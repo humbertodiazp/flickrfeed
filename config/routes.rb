e96b3272c3a0983ab5ff13069723b604fc966882
Humberto <hdkaratechop@gmail.com>
2023-03-01 22:19:01 -0800

    change root to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   resources :staticpages
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-   root "staticpages#new"
+  root "staticpages#index"
 end

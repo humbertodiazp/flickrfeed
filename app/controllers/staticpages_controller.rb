ade9269d9c20040c34fb49834e74fae052ad4849
Humberto <hdkaratechop@gmail.com>
2023-03-01 21:14:47 -0800

    revert to user_id

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -1,11 +1,10 @@
 class StaticpagesController < ApplicationController
-  require "flickr"
 
   def index
     flickr = Flickr.new(ENV['FLICKR_API_KEY'], ENV['FLICKR_SHARED_SECRET'])
     user_id = params[:flickr_id]
     begin
-        @photo_urls = flickr.people.getPublicPhotos(user_id: flickr_id)
+        @photo_urls = flickr.people.getPublicPhotos(user_id: user_id)
     rescue Flickr::FailedResponse => e
     end
   end
